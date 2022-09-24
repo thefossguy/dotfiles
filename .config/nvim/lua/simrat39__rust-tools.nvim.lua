7c55b820d7f40d3be1bfcfc7d44e69d6e9c704d7
Pratham Patel <thefirst1322@gmail.com>
2022-09-24 22:57:23 +0530

    add inlay hints for Rust

--- .config/nvim/lua/simrat39__rust-tools.nvim.lua
+++ .config/nvim/lua/simrat39__rust-tools.nvim.lua
@@ -168,3 +168,18 @@ local opts = {
 }
 
 require('rust-tools').setup(opts)
+
+-- Set inlay hints for the current buffer
+require('rust-tools').inlay_hints.set()
+-- Unset inlay hints for the current buffer
+require('rust-tools').inlay_hints.unset()
+-- Enable inlay hints auto update and set them for all the buffers
+require('rust-tools').inlay_hints.enable()
+-- Disable inlay hints auto update and unset them for all buffers
+require('rust-tools').inlay_hints.disable()
+
+-- RustRunnables
+require('rust-tools').runnables.runnables()
+
+-- RustExpandMacro
+require'rust-tools'.expand_macro.expand_macro()
