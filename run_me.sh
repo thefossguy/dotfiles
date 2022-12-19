221bc8e1eb6881f714c9fd17b5f73fe922368803
Pratham Patel <thefirst1322@gmail.com>
2022-12-19 23:21:08 +0530

    added a script to copy dotfiles

---
+++ run_me.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+rsync \
+    --verbose --recursive --size-only --human-readable \
+    --progress --stats \
+    --itemize-changes --checksum \
+    --exclude=".git" --exclude=".gitignore" --exclude="README.md" --exclude="run_me.sh" \
+    ../dotfiles/ ~/ --dry-run
