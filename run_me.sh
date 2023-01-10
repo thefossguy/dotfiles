0c8354172b144ed633087e62b8370aecf2d67d1b
Pratham Patel <thefirst1322@gmail.com>
2023-01-10 08:40:07 +0530

    rsync: use --perms

--- run_me.sh
+++ run_me.sh
@@ -3,6 +3,6 @@
 rsync \
     --verbose --recursive --size-only --human-readable \
     --progress --stats \
-    --itemize-changes --checksum \
+    --itemize-changes --checksum --perms \
     --exclude=".git" --exclude=".gitignore" --exclude="README.md" --exclude="run_me.sh" \
     ../dotfiles/ ~/ --dry-run
