4c9c7da332503e1136a9d53bab05b4bc4ea461c1
Pratham Patel <thefirst1322@gmail.com>
2023-01-11 10:52:39 +0530

    idk why I removed rsync's --perms param

--- run_me.sh
+++ run_me.sh
@@ -3,6 +3,6 @@
 rsync \
     --verbose --recursive --size-only --human-readable \
     --progress --stats \
-    --itemize-changes --checksum --perms \
+    --itemize-changes --checksum \
     --exclude=".git" --exclude=".gitignore" --exclude="README.md" --exclude="run_me.sh" \
     ../dotfiles/ ~/ --dry-run
