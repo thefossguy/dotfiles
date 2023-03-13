6e936ce8bb2b672bbacf96408b1304d509399972
Pratham Patel <thefirst1322@gmail.com>
2023-03-13 16:34:34 +0530

    run_me.sh: do not copy dir '_OTHER'

--- run_me.sh
+++ run_me.sh
@@ -4,5 +4,6 @@ rsync \
     --verbose --recursive --size-only --human-readable \
     --progress --stats \
     --itemize-changes --checksum \
-    --exclude=".git" --exclude=".gitignore" --exclude="README.md" --exclude="run_me.sh" \
+    --exclude=".git" --exclude=".gitignore" --exclude="README.md" \
+    --exclude="run_me.sh" --exclude="_OTHER" \
     ../dotfiles/ ~/ --dry-run
