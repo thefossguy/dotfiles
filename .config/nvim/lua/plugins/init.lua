c605a7203638108f18a77e4b3237996d1728930e
Pratham Patel <thefirst1322@gmail.com>
2023-06-04 18:58:19 +0530

    nvim: refactor plugins/init.lua
     - add and remove plugins
     - group plugins together
     - move as much as plugin setup as I can out of plugins/init.lua

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,19 +1,20 @@
 --------------------------------------------------------------------------------
--- setup package manager
+-- setup the plugin manager
+-- https://github.com/folke/lazy.nvim
 --------------------------------------------------------------------------------
 
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
+local lazy_path = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazy_path) then
   vim.fn.system {
     'git',
     'clone',
     '--filter=blob:none',
     'https://github.com/folke/lazy.nvim.git',
     '--branch=stable', -- latest stable release
-    lazypath,
+    lazy_path,
   }
 end
-vim.opt.rtp:prepend(lazypath)
+vim.opt.rtp:prepend(lazy_path)
 
 
 --------------------------------------------------------------------------------
@@ -22,149 +23,134 @@ vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
 
-  -- git related plugins
-  'tpope/vim-fugitive',
-  'tpope/vim-rhubarb',
+  {
+    -- theme
+    'catppuccin/nvim',
+    name = 'catppuccin',
+  },
 
   -- detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
 
-  -- theme
-  { "catppuccin/nvim", name = "catppuccin" },
+  {
+    -- programming/scripting langauges
 
-  { -- Rust lang
-    'rust-lang/rust.vim',
-    'simrat39/rust-tools.nvim',
+    'm4xshen/autoclose.nvim', -- autoclose brackets and quotes
 
-    -- debugging
-    'nvim-lua/plenary.nvim',
-    'mfussenegger/nvim-dap',
-
-    -- for crates.io
     {
-      'Saecki/crates.nvim',
-      tag = 'v0.3.0',
-      config = function()
-        require('crates').setup()
-      end
-    },
+      -- Rust
+      'rust-lang/rust.vim',
+      'simrat39/rust-tools.nvim',
+
+      -- debugging
+      'nvim-lua/plenary.nvim',
+      'mfussenegger/nvim-dap',
+    }
+
+      -- Python
+      -- Bash
+      -- C/C++
+      -- Lua
+
   },
 
-  { -- tree view
-    'nvim-tree/nvim-tree.lua',
-    'nvim-tree/nvim-web-devicons',
-  },
-
-  { -- LSP configuration and plugins
+  {
+    -- LSP
     'neovim/nvim-lspconfig',
     dependencies = {
-      -- automatically install LSPs to stdpath for neovim
-      'williamboman/mason.nvim',
-      'williamboman/mason-lspconfig.nvim',
-
-      -- useful status updates for LSP
-      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
-
-      -- additional lua configuration, makes nvim stuff amazing
-      'folke/neodev.nvim',
+      'folke/neodev.nvim', -- additional lua configuration, makes nvim stuff amazing
+      'j-hui/fidget.nvim', -- useful status updates for LSP
     },
   },
 
-  { -- autocompletion
+  {
+    -- autocompletion
     'hrsh7th/nvim-cmp',
-    dependencies = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
-  },
-
-  -- useful plugin to show you pending keybinds
-  { 'folke/which-key.nvim', opts = {} },
-  { -- adds git releated signs to the gutter, as well as utilities for managing changes
-    'lewis6991/gitsigns.nvim',
-    opts = {
-      -- see `:help gitsigns.txt`
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-        untracked = { text = '' },
+    dependencies = {
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-nvim-lua',
+      'hrsh7th/cmp-path',
+      'saadparwaiz1/cmp_luasnip',
+      'windwp/nvim-autopairs',
+      {
+        -- snippet plugin
+        'L3MON4D3/LuaSnip',
+        dependencies = 'rafamadriz/friendly-snippets',
       },
     },
   },
 
-  { -- set lualine as statusline
-    'nvim-lualine/lualine.nvim',
-    -- see `:help lualine.txt`
-    opts = {
-      options = {
-        icons_enabled = true,
-        theme = 'nightfly',
-        globalstatus = true,
-        component_separators = { left = '', right = ''},
-        section_separators = { left = '', right = ''},
-        always_divide_middle = false,
-        refres = {
-          statusline = 1000,
-          tabline = 1000,
-          winbar = 1000,
-        },
-      },
-      sections = {
-        lualine_a = {'mode'},
-        lualine_b = {'branch', 'diff'},
-        lualine_c = {
-          {
-            'filename',
-            file_status = true,
-            icon_only = true,
-            symbols = {
-              modified = '[+]',
-              readonly = '[RO]',
-              unnamed = '[NO NAME]',
-              newfile = '[NEW FILE]',
-            },
-          }, {
-            'diagnostics',
-            sources = { 'nvim_lsp' },
-            sections = { 'error', 'warn', 'info', 'hint' },
-            colored = true,
-            always_visible = true,
-          }
-        },
-        lualine_x = {'encoding', 'fileformat', 'filetype'},
-        lualine_y = {'searchcount', 'progress'},
-        lualine_z = {'location'},
-      },
+  {
+    -- telescope
+    'nvim-telescope/telescope.nvim',
+    'gregorias/nvim-mapper', -- shows keymaps
+    'nvim-lua/popup.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
     },
   },
 
-  { -- add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- enable `lukas-reineke/indent-blankline.nvim`
-    -- see `:help indent_blankline.txt`
-    --[[
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-    },
-    --]]
-  },
-
-  -- 'gc' to comment visual regions/lines
-  { 'numToStr/Comment.nvim', opts = {} },
-
-  -- fuzzy finder (files, lsp, etc)
-  { 'nvim-telescope/telescope.nvim', version = '*', dependencies = { 'nvim-lua/plenary.nvim' } },
-
-  { -- highlight, edit, and navigate code
+  {
+    -- treesitter
     'nvim-treesitter/nvim-treesitter',
     dependencies = {
       'nvim-treesitter/nvim-treesitter-textobjects',
     },
     config = function()
-      pcall(require('nvim-treesitter.install').update { with_sync = true })
-    end,
+      pcall(require('nvim-treesitter.install').update({
+        with_sync = true
+      }))
+    end
   },
 
+  -- adds git signs to the gutter, as well as utilities for managing changes
+  'lewis6991/gitsigns.nvim',
+
+  -- incrementally rename
+  'smjonas/inc-rename.nvim',
+
+  {
+    -- tree view
+    'nvim-tree/nvim-tree.lua',
+    'nvim-tree/nvim-web-devicons',
+  },
+
+  -- set lualine as statusline
+  'nvim-lualine/lualine.nvim',
+
+  -- show a pop-up with possible **key-bindings** of the command you started typing
+  'folke/which-key.nvim',
+
+  {
+    -- notifications
+    'folke/noice.nvim',
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+      'rcarriga/nvim-notify',
+    }
+  },
+
+  -- add indentation guides even on blank lines
+  'lukas-reineke/indent-blankline.nvim',
+
+  -- 'gc' to comment visual regions/lines
+  'numToStr/Comment.nvim',
+
 }, {})
+
+
+--------------------------------------------------------------------------------
+-- setup the plugins
+--------------------------------------------------------------------------------
+
+require('plugins/theme')
+require('plugins/lualine')
+require('plugins/gitsigns')
+require('plugins/tree-view')
+require('plugins/notifs') -- nvim-notify, noice.nvim
+require('plugins/other') -- Comment.nvim, indent-blankline.nvim
+
+require('plugins/autocomplete') -- autocomplete + LSP
+require('plugins/debugging')
+-- require('plugins/')
