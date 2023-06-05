9fbbf11179963767f07386ee4fe212c7de33f55e
Pratham Patel <thefirst1322@gmail.com>
2023-06-05 10:36:05 +0530

    sort.py: update the script to work with cmdline args and also pipes (stdin)

--- .local/scripts/sort.py
+++ .local/scripts/sort.py
@@ -3,20 +3,26 @@
 import sys
 import os
 
+INPUT_STREAM = os.fstat(0)
+OUTPUT_STREAM = os.fstat(1)
 
-def mk_list():
+unsorted = ""
 
-    input_args = ""
-    for i in sys.argv:
-        input_args = input_args + " " + i
-
-    input_args = list(dict.fromkeys(input_args.split(" ")))
-
-    del input_args[0]
-    del input_args[0]
-    input_args.sort()
-
-    return input_args
-
-to_print = mk_list()
-print('[ %s ]' % ' '.join(map(str, to_print)))
+if len(sys.argv) > 1:
+    # accept the input as command line arguments
+    for argument in sys.argv:
+        unsorted = unsorted + " " + argument
+    unsorted = list(dict.fromkeys(unsorted.split(" ")))
+    del unsorted[0] # remove the executable name
+    del unsorted[0] # remove the space added by the above `.split()`
+    unsorted.sort()
+    print('[ %s ]' % ' '.join(map(str, unsorted)))
+else:
+    # accept input from STDIN
+    for line in sys.stdin:
+        line = line.strip()
+        unsorted = unsorted + " " + line
+    unsorted = list(dict.fromkeys(unsorted.split(" ")))
+    del unsorted[0] # remove the space added by the above `.split()`
+    unsorted.sort()
+    print('%s' % ' '.join(map(str, unsorted)))
