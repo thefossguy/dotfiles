856d77eb701fd0c31d2bc2b152e574989e7b53db
Pratham Patel <thefirst1322@gmail.com>
2023-06-18 19:20:34 +0530

    sort.py: improve TTY detection

--- .local/scripts/other-common-scripts/sort.py
+++ .local/scripts/other-common-scripts/sort.py
@@ -3,9 +3,6 @@
 import sys
 import os
 
-INPUT_STREAM = os.fstat(0)
-OUTPUT_STREAM = os.fstat(1)
-
 unsorted = ""
 
 if len(sys.argv) > 1:
@@ -16,7 +13,6 @@ if len(sys.argv) > 1:
     del unsorted[0] # remove the executable name
     del unsorted[0] # remove the space added by the above `.split()`
     unsorted.sort()
-    print('[ %s ]' % ' '.join(map(str, unsorted)))
 else:
     # accept input from STDIN
     for line in sys.stdin:
@@ -25,4 +21,8 @@ else:
     unsorted = list(dict.fromkeys(unsorted.split(" ")))
     del unsorted[0] # remove the space added by the above `.split()`
     unsorted.sort()
+
+if sys.stdout.isatty():
+    print('[ %s ]' % ' '.join(map(str, unsorted)))
+else:
     print('%s' % ' '.join(map(str, unsorted)))
