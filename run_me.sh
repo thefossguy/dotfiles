37b52999c85ce9fba025f3bcb664e7b468261990
Pratham Patel <thefirst1322@gmail.com>
2023-06-18 09:04:19 +0530

    remove README.md run_me.sh because it is no longer needed

--- run_me.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-rsync \
-    --verbose --recursive --size-only --human-readable \
-    --progress --stats \
-    --itemize-changes --checksum \
-    --exclude=".git" --exclude=".gitignore" --exclude="README.md" \
-    --exclude="run_me.sh" --exclude="_OTHER" \
-    ../dotfiles/ ~/ --dry-run
