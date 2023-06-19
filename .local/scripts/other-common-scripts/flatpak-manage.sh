42f24d0cf2463861679dd5a477961c2be514a5a3
Pratham Patel <thefirst1322@gmail.com>
2023-06-19 15:18:36 +0530

    run flatpak-manage.sh only if flatpak is present

--- .local/scripts/other-common-scripts/flatpak-manage.sh
+++ .local/scripts/other-common-scripts/flatpak-manage.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 
-flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak repair --user
-flatpak update --user --assumeyes --noninteractive
-flatpak install --user --or-update --assumeyes --noninteractive \
-    com.bitwarden.desktop                $(: # Bitwarden) \
-    com.brave.Browser                    $(: # Brave) \
-    com.discordapp.Discord               $(: # Discord) \
-    com.github.tchx84.Flatseal           $(: # Flatseal; OFFICIAL) \
-    fr.handbrake.ghb                     $(: # Handbrake; OFFICIAL) \
-    io.gitlab.librewolf-community        $(: # LibreWolf) \
-    io.mpv.Mpv                           $(: # MPV) \
-    org.gnome.gitlab.YaLTeR.Identity     $(: # Identity \(compare images\); OFFICIAL) \
-    org.gnome.gitlab.YaLTeR.VideoTrimmer $(: # Video Trimmer; OFFICIAL) \
-    org.gnome.Logs                       $(: # Logs; OFFICIAL) \
-    org.gnome.meld                       $(: # Meld (shows diff)) \
-    org.mozilla.firefox                  $(: # Firefox; OFFICIAL) \
-    org.raspberrypi.rpi-imager           $(: # Raspberry Pi Imager)
-flatpak uninstall --user --unused --assumeyes --noninteractive --delete-data
+if command -q flatpak; then
+    flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    flatpak repair --user
+    flatpak update --user --assumeyes --noninteractive
+    flatpak install --user --or-update --assumeyes --noninteractive \
+        com.bitwarden.desktop                $(: # Bitwarden) \
+        com.brave.Browser                    $(: # Brave) \
+        com.discordapp.Discord               $(: # Discord) \
+        com.github.tchx84.Flatseal           $(: # Flatseal; OFFICIAL) \
+        fr.handbrake.ghb                     $(: # Handbrake; OFFICIAL) \
+        io.gitlab.librewolf-community        $(: # LibreWolf) \
+        io.mpv.Mpv                           $(: # MPV) \
+        org.gnome.gitlab.YaLTeR.Identity     $(: # Identity \(compare images\); OFFICIAL) \
+        org.gnome.gitlab.YaLTeR.VideoTrimmer $(: # Video Trimmer; OFFICIAL) \
+        org.gnome.Logs                       $(: # Logs; OFFICIAL) \
+        org.gnome.meld                       $(: # Meld (shows diff)) \
+        org.mozilla.firefox                  $(: # Firefox; OFFICIAL) \
+        org.raspberrypi.rpi-imager           $(: # Raspberry Pi Imager)
+    flatpak uninstall --user --unused --assumeyes --noninteractive --delete-data
+fi
