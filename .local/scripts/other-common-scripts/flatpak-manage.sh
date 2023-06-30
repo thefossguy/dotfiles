b4f5259d50435f1b64a064432ffce54651fcd4e5
Pratham Patel <thefirst1322@gmail.com>
2023-06-30 18:12:49 +0530

    move pkgs from flatpak to nixos since there are no arm builds (at least not yet)

--- .local/scripts/other-common-scripts/flatpak-manage.sh
+++ .local/scripts/other-common-scripts/flatpak-manage.sh
@@ -3,18 +3,15 @@
 if command -v flatpak > /dev/null; then
     flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
     flatpak install --user --or-update --assumeyes --noninteractive \
-        com.bitwarden.desktop                \
         com.brave.Browser                    \
         com.discordapp.Discord               \
         com.github.tchx84.Flatseal           \
         fr.handbrake.ghb                     \
         io.gitlab.librewolf-community        \
-        io.mpv.Mpv                           \
         org.gnome.gitlab.YaLTeR.Identity     \
         org.gnome.gitlab.YaLTeR.VideoTrimmer \
         org.gnome.Logs                       \
         org.gnome.meld                       \
-        org.mozilla.firefox                  \
         org.raspberrypi.rpi-imager
     flatpak update --user --assumeyes --noninteractive
     flatpak uninstall --user --unused --assumeyes --noninteractive --delete-data
