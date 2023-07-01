33a83790eea395c7b06b260c0e7a213ce665f9f9
Pratham Patel <thefirst1322@gmail.com>
2023-07-01 13:27:34 +0530

    rust-manage: add components rust-analyzer and clippy; enable autocompletions for rust in the fish shell

--- .local/scripts/other-common-scripts/rust-manage.sh
+++ .local/scripts/other-common-scripts/rust-manage.sh
@@ -2,4 +2,9 @@
 
 rustup default stable
 rustup update stable
-rustup component add rust-src rust-analysis
+rustup component add rust-src rust-analysis rust-analyzer clippy
+
+if [[ ! -d "$HOME/.config/fish/completions" ]]; then
+    mkdir -p "$HOME/.config/fish/completions"
+fi
+rustup completions fish > "$HOME/.config/fish/completions/rustup.fish"
