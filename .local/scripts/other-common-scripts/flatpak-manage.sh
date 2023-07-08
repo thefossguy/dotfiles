47d2f64dd3a58fd947f44f4a40431cc581827d8f
Pratham Patel <thefirst1322@gmail.com>
2023-07-09 01:31:48 +0530

    improve the flatpak install/update script

--- .local/scripts/other-common-scripts/flatpak-manage.sh
+++ .local/scripts/other-common-scripts/flatpak-manage.sh
@@ -1,18 +1,34 @@
 #!/usr/bin/env bash
 
+function install_flatpaks_flameboi() {
+    flatpak install --user --or-update --assumeyes --noninteractive \
+        com.brave.Browser \
+        com.discordapp.Discord \
+        com.github.tchx84.Flatseal \
+        fr.handbrake.ghb \
+        io.gitlab.librewolf-community \
+        org.gnome.gitlab.YaLTeR.Identity \
+        org.gnome.gitlab.YaLTeR.VideoTrimmer \
+        org.gnome.Logs \
+        org.gnome.meld \
+        org.raspberrypi.rpi-imager \
+        #EOF
+}
+
+function install_flatpaks_vm() {
+    flatpak install --user --or-update --assumeyes --noninteractive \
+        com.brave.Browser \
+        io.gitlab.librewolf-community \
+        #EOF
+}
+
 if command -v flatpak > /dev/null; then
     flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    flatpak install --user --or-update --assumeyes --noninteractive \
-        com.brave.Browser                    \
-        com.discordapp.Discord               \
-        com.github.tchx84.Flatseal           \
-        fr.handbrake.ghb                     \
-        io.gitlab.librewolf-community        \
-        org.gnome.gitlab.YaLTeR.Identity     \
-        org.gnome.gitlab.YaLTeR.VideoTrimmer \
-        org.gnome.Logs                       \
-        org.gnome.meld                       \
-        org.raspberrypi.rpi-imager
+    if [[ ${HOSTNAME} == "flameboi" ]]; then
+        install_flatpaks_flameboi
+    elif [[ ${HOSTNAME} =~ "virt" || ${HOSTNAME} =~ "nix" || ${HOSTNAME} =~ "vm" ]]; then
+        install_flatpaks_vm
+    fi
     flatpak update --user --assumeyes --noninteractive
     flatpak uninstall --user --unused --assumeyes --noninteractive --delete-data
     flatpak repair --user
