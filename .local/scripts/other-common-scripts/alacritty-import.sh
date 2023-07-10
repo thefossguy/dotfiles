05f6ca7bdf90a8af9bf41f5e4e9316770e4bb0c7
Pratham Patel <thefirst1322@gmail.com>
2023-07-10 07:34:02 +0530

    fix alacritty imports

--- .local/scripts/other-common-scripts/alacritty-import.sh
+++ .local/scripts/other-common-scripts/alacritty-import.sh
@@ -2,8 +2,8 @@
 
 if [ ! -f "${HOME}/.config/alacritty/load_intermediate.yml" ]; then
     if uname | grep -q Linux; then
-        echo "load_linux.yml" > "${HOME}/.config/alacritty/load_intermediate.yml"
+        ln -s "${HOME}/.config/alacritty/load_linux.yml" "${HOME}/.config/alacritty/load_intermediate.yml"
     else
-        echo "load_macos.yml" > "${HOME}/.config/alacritty/load_intermediate.yml"
+        ln -s "${HOME}/.config/alacritty/load_macos.yml" "${HOME}/.config/alacritty/load_intermediate.yml"
     fi
 fi
