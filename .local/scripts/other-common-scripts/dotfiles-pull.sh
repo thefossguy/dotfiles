4401f37b9a62f2f109e0d620f61501aac4bd7828
Pratham Patel <thefirst1322@gmail.com>
2023-08-26 14:09:03 +0530

    add scripts to pull dotfiles and nixos config

---
+++ .local/scripts/other-common-scripts/dotfiles-pull.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i dash --packages dash git
+
+set -xeu
+
+DIRTY_REPO=$(git --git-dir="${HOME}/.dotfiles" --work-tree="${HOME}" status --porcelain=v1 --ignored=no --untracked-files=no)
+if [ -n "${DIRTY_REPO}" ]; then
+    >&2 echo "$0: You have unsaved changes, not pulling..."
+    exit 1
+fi
+git --git-dir="${HOME}/.dotfiles" --work-tree="${HOME}" pull
---
+++ .local/scripts/other-common-scripts/nixos-config-pull.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env nix-shell
+#!nix-shell -i dash --packages dash git
+
+set -xeu
+
+GIT_REPO_LOCAL_PATH="${HOME}/my-git-repos/pratham/prathams-nixos/"
+cd "${GIT_REPO_LOCAL_PATH}"
+DIRTY_REPO=$(git status --porcelain=v1 --ignored=no --untracked-files=no)
+if [ -n "${DIRTY_REPO}" ]; then
+    >&2 echo "$0: You have unsaved changes, not pulling..."
+    exit 1
+fi
+git pull
