440d2a395d8bde76a8df8ffd9c95b7d815dc087a
Pratham Patel <thefirst1322@gmail.com>
2023-08-31 20:23:23 +0530

    rust-manage.sh: switch "interpreter" to nix-shell for being used by systemd service on NixOS

--- .local/scripts/other-common-scripts/rust-manage.sh
+++ .local/scripts/other-common-scripts/rust-manage.sh
@@ -1,10 +1,19 @@
-#!/usr/bin/env bash
+#!/usr/bin/env nix-shell
+#!nix-shell -i dash --packages dash rustup
+
+set -xeu
+
+if pgrep "rust-analyzer|cargo|rustc" > /dev/null; then
+    >&2 echo "$0: You are probably using components that will be updated, even replaced..."
+    >&2 echo "$0: Not continuing"
+    exit 1
+fi
 
 rustup default stable
 rustup update stable
 rustup component add rust-src rust-analysis rust-analyzer clippy
 
-if [[ ! -d "$HOME/.config/fish/completions" ]]; then
-    mkdir -p "$HOME/.config/fish/completions"
+if [ ! -d "${HOME}/.config/fish/completions" ]; then
+    mkdir -p "${HOME}/.config/fish/completions"
 fi
-rustup completions fish > "$HOME/.config/fish/completions/rustup.fish"
+rustup completions fish > "${HOME}/.config/fish/completions/rustup.fish"
