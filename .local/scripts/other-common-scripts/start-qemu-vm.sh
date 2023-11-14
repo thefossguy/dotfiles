03253341c4b32f5bf3e4fd24e9cf81ac71bdb193
Pratham Patel <thefirst1322@gmail.com>
2023-11-14 14:35:24 +0530

    start-qemu-vm.sh: moar common

--- .local/scripts/other-common-scripts/start-qemu-vm.sh
+++ .local/scripts/other-common-scripts/start-qemu-vm.sh
@@ -24,24 +24,25 @@ if [ ! -f "${HOME}/.vms/result/u-boot.bin" ]; then
     popd
 fi
 
-QEMU_COMMON="-machine virt \
-    -cpu host \
-    -accel kvm \
-    -m 8192 \
-    -nographic \
-    -bios ${HOME}/.vms/result/u-boot.bin \
-    -sandbox on \
-    -netdev user,id=mynet0,hostfwd=tcp::6969-:22 \
-    -device virtio-net-pci,netdev=mynet0"
+QEMU_COMMON="--all-tasks --cpu-list 4-7 \
+    qemu-kvm \
+        -machine virt \
+        -cpu host \
+        -smp 4,sockets=1,cores=4,threads=1 \
+        -accel kvm \
+        -m 8192 \
+        -nographic \
+        -bios ${HOME}/.vms/result/u-boot.bin \
+        -sandbox on \
+        -netdev user,id=mynet0,hostfwd=tcp::6969-:22 \
+        -device virtio-net-pci,netdev=mynet0"
 if [ -z "$2" ]; then
-    taskset --cpu-list 4-7 \
-        qemu-kvm \
-            ${QEMU_COMMON} \
-            -hda "$1"
+    taskset \
+        ${QEMU_COMMON} \
+        -hda "$1"
 else
-    taskset --cpu-list 4-7 \
-        qemu-kvm \
-            ${QEMU_COMMON} \
-            -hda "$2" \
-            -hdb "$1"
+    taskset \
+        ${QEMU_COMMON} \
+        -hda "$2" \
+        -hdb "$1"
 fi
