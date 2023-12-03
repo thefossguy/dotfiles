b2e88ff2bf5d23d62bc4483284b21e454ea0a59f
Pratham Patel <thefirst1322@gmail.com>
2023-12-03 11:10:31 +0530

    kernel-opt-compare.sh: add script that helps me find "missing" options from Linux kernel config

---
+++ .local/scripts/other-common-scripts/kernel-opt-compare.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+if [[ -z "$1" ]]; then
+    echo "What do you want to grep for, bud?"
+    exit 1
+fi
+
+if [[ -z "$2" ]]; then
+    echo "What's the SRC?"
+    exit 1
+fi
+
+if [[ -z "$3" ]]; then
+    echo "What's the DST?"
+    exit 1
+fi
+
+SRC_FILE="$2"
+SRC_MATCHES=($(grep -o "CONFIG_.*$1.*\ i\|CONFIG_.*$1.*=[m,y]" "${SRC_FILE}" | tr " i" ".i"))
+
+DST_FILE="$3"
+DST_MATCHES=($(grep -o "CONFIG_.*$1.*\ i\|CONFIG_.*$1.*=[m,y]" "${DST_FILE}" | tr " i" ".i"))
+
+ALL_CONFIGS=()
+
+for OPTION in "${SRC_MATCHES[@]}" "${DST_MATCHES[@]}"; do
+    NEW_OPTION=$(echo "${OPTION}" | rev | cut -c 3- | rev)
+    ALL_CONFIGS+=( "${NEW_OPTION}" )
+done
+
+NEW_CONFIGS=($(echo "${ALL_CONFIGS[@]}" | tr " " "\n" | sort | uniq))
+
+for OPTION in "${NEW_CONFIGS[@]}"; do
+    echo '----'
+    echo -n 'SRC: ' && (grep --color=yes "${OPTION}" "$SRC_FILE" || echo "")
+    echo -n 'DST: ' && (grep --color=yes "${OPTION}" "$DST_FILE" || echo "")
+done
