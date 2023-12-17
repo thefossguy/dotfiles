6523c1bb4691a83ba7c34ff193fa5aaee853efda
Pratham Patel <thefirst1322@gmail.com>
2023-12-17 11:50:56 +0530

    upgrade-nixos.sh: display some nice messages since I will be checking the service log

--- .local/scripts/nixos/upgrade-nixos.sh
+++ .local/scripts/nixos/upgrade-nixos.sh
@@ -4,8 +4,10 @@ set -xeuf -o pipefail
 
 if [[ "$(nix eval -f '<nixpkgs>' 'linux.version' | sed -e 's/"//g')" == "$(uname -r)" ]]; then
     KERNEL_PACKAGE='linux'
+    echo 'LTS kernel detected.'
 elif [[ "$(nix eval -f '<nixpkgs>' 'linux_latest.version' | sed -e 's/"//g')" == "$(uname -r)" ]]; then
     KERNEL_PACKAGE='linux_latest'
+    echo 'Stable kernel detected.'
 else
     echo 'Could not determine which kernel you are using. Bye!'
     exit 1
@@ -16,12 +18,14 @@ nix-channel --update nixos
 NEW_KERNEL_VERSION="$(nix eval -f '<nixpkgs>' ${KERNEL_PACKAGE}.version | sed -e 's/"//g')"
 if [[ "${OLD_KERNEL_VERSION}" != "${NEW_KERNEL_VERSION}" ]]; then
     KERNEL_UPGRADE=true
+    echo 'Kernel upgrade available!'
 else
     KERNEL_UPGRADE=false
 fi
 
 if [[ "$(date +%A)" == 'Saturday' ]]; then
     UNCONDITIONAL_UPGRADE=true
+    echo 'Upgrading NixOS unconditionally.'
 else
     UNCONDITIONAL_UPGRADE=false
 fi
@@ -29,5 +33,5 @@ fi
 if [[ "${KERNEL_UPGRADE}" || "${UNCONDITIONAL_UPGRADE}" ]]; then
     nixos-rebuild boot --upgrade-all
 else
-    echo 'Nothing to upgrade today.'
+    echo '*yawn*'
 fi
