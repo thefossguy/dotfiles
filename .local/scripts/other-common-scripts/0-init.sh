79814f7cbe92c968cb79dbafeb45b5b7cbca5a7e
Pratham Patel <thefirst1322@gmail.com>
2024-01-07 17:36:32 +0530

    rename script to perform a single-time setup and also add fetching of git-prompt.sh

--- .local/scripts/other-common-scripts/0-init.sh
+++ .local/scripts/other-common-scripts/0-init.sh
@@ -9,3 +9,9 @@ if [[ ! -f "${HOME}/.config/alacritty/platform.yml" ]]; then
         ln -s "${HOME}/.config/alacritty/"{darwin,platform}.yml
     fi
 fi
+
+if [[ ! -f "${HOME}/.local/scripts/other-common-scripts/git-prompt.sh" ]]; then
+    pushd "${HOME}/.local/scripts/other-common-scripts"
+    wget 'https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh'
+    popd
+fi
