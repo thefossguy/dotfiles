c5f9f40ba5b33e363ac0766cb60f4e939fd6e4fd
Pratham Patel <thefirst1322@gmail.com>
2024-02-11 16:06:05 +0530

    home-manger: add service+timer to update the home-manager flake.lock

--- .config/home-manager/linux.nix
+++ .config/home-manager/linux.nix
@@ -7,4 +7,42 @@ in
   home.username = "${me}";
   home.homeDirectory = "/home/${me}";
   targets.genericLinux.enable = true;
+
+  systemd.user = {
+    timers = {
+      "update-flake-inputs" = {
+        Unit = {
+          Description = "Flake input update timer";
+        };
+        Timer = {
+          OnCalendar = "*** 23:30:00";
+          Persistent = "true";
+          Unit = "update-flake-inputs.service";
+        };
+        Install = {
+          WantedBy = [ "timers.target" ];
+        };
+      };
+    };
+    services = {
+      "update-flake-inputs" = {
+        Unit = {
+          Description = "Update flake inputs for Home Manager";
+          Before = [ "home-manager-auto-upgrade.service" ];
+        };
+        Service = {
+          ExecStart = toString
+            (pkgs.writeShellScript "" ''
+              echo 'Running `nix flake update` for the Home Manager flake'
+              pushd $HOME/.config/home-manager
+              ${pkgs.nix}/bin/nix flake update
+              popd
+            '');
+        };
+        Install = {
+          RequiredBy = [ "home-manager-auto-upgrade.service" ];
+        };
+      };
+    };
+  };
 }
