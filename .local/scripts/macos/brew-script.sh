0eb75e766693792d10c3b9e32a3fe29d1888387f
Pratham Patel <thefirst1322@gmail.com>
2024-02-19 12:29:29 +0530

    brew-script.sh: init commit

---
+++ .local/scripts/macos/brew-script.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+set -xeuf
+
+# **INSTALL ONLY WHAT DOESN'T SEEM TO WORK WITH NIX/HOME-MANAGER**
+BREW_FORUMLAS=(
+    bash
+    mpv
+    neovide
+    rpm2cpio
+    wakeonlan
+)
+
+# "GUI apps" that cannot be handled by home-manager
+BREW_CASKS=(
+    alacritty
+    balenaetcher
+    bitwarden
+    brave-browser
+    discord
+    exifcleaner
+    firefox
+    google-chrome
+    handbrake
+    keka
+    libreoffice
+    librewolf
+    maccy
+    macs-fan-control
+    meld
+    obs
+    protonvpn
+    raspberry-pi-imager
+    slack
+    thunderbird
+    tor-browser
+    utm
+    whatsapp
+)
+
+brew analytics off
+brew update --force # upgrade homebrew itself
+brew upgrade --greedy --greedy-latest --greedy-auto-updates --no-quarantine # upgrade the packages installed by homebrew
+
+brew install --forumla "${BREW_FORUMLAS[@]}"
+brew install --cask "${BREW_CASKS[@]}"
+
+brew autoremove
+brew cleanup --prune=all -s
+brew doctor
+brew outdated --greedy --greedy-latest --greedy-auto-updates
+brew missing
