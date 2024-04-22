f8240df1379e29830de7b8a43018942db62e53b0
Pratham Patel <prathampatel@thefossguy.com>
2024-04-22 07:32:05 +0530

    flatpak-manage.sh: better handle browser permissions

--- .local/scripts/other-common-scripts/flatpak-manage.sh
+++ .local/scripts/other-common-scripts/flatpak-manage.sh
@@ -2,10 +2,16 @@
 
 set -xeuf -o pipefail
 
-COMMON_PKGS=(
+BROWSERS_AMD=(
+    com.google.Chrome
+)
+BROWSERS_COMMON=(
     com.brave.Browser
-    com.github.tchx84.Flatseal
     io.gitlab.librewolf-community
+)
+COMMON_PKGS=(
+    "${BROWSERS_COMMON[@]}"
+    com.github.tchx84.Flatseal
     md.obsidian.Obsidian
     org.gnome.gitlab.YaLTeR.Identity
     org.gnome.gitlab.YaLTeR.VideoTrimmer
@@ -14,19 +20,26 @@ COMMON_PKGS=(
     org.raspberrypi.rpi-imager
 )
 AMD_PKGS=(
+    "${COMMON_PKGS[@]}"
+    "${BROWSERS_AMD[@]}"
     com.discordapp.Discord
     fr.handbrake.ghb
 )
-ARM_PKGS=()
-RISCV_PKGS=() # lol
+ARM_PKGS=(
+    "${COMMON_PKGS[@]}"
+)
+RISCV_PKGS=(
+    "${COMMON_PKGS[@]}"
+)
+BROWSERS_ALL=( "${BROWSERS_AMD[@]}" "${BROWSERS_COMMON[@]}" )
 
 if [[ -d '/sys' ]]; then
     if grep 'x86_64' /proc/sys/kernel/arch > /dev/null; then
-        ALL_PKGS=( "${COMMON_PKGS[@]}" "${AMD_PKGS[@]}" )
+        ALL_PKGS=( "${AMD_PKGS[@]}" )
     elif grep 'aarch64' /proc/sys/kernel/arch > /dev/null; then
-        ALL_PKGS=( "${COMMON_PKGS[@]}" "${ARM_PKGS[@]}" )
+        ALL_PKGS=( "${ARM_PKGS[@]}" )
     elif grep 'riscv64' /proc/sys/kernel/arch > /dev/null; then
-        ALL_PKGS=( "${COMMON_PKGS[@]}" "${RISCV_PKGS[@]}" )
+        ALL_PKGS=( "${RISCV_PKGS[@]}" )
     else
         echo 'Unsupported CPU ISA'
         exit 1
@@ -50,3 +63,11 @@ ${FLATPAK_BIN} install --user --or-update --assumeyes --noninteractive "${ALL_PK
 ${FLATPAK_BIN} update --user --assumeyes --noninteractive
 ${FLATPAK_BIN} uninstall --user --unused --assumeyes --noninteractive --delete-data
 ${FLATPAK_BIN} repair --user
+
+# Brave is in nixpkgs 24.05 and later
+# no need to use a browser as a flatpak except for filesystem ($HOME) sandboxing
+for flatpak_pkg in "${ALL_PKGS[@]}"; do
+    if [[ "${BROWSERS_ALL[*]}" == *"${flatpak_pkg}"* ]]; then
+        flatpak override --user --reset "${flatpak_pkg}"
+    fi
+done
