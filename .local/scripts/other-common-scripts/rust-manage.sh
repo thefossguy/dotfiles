8907fd4ca3b752d45da9d767aa3e3c9b17e8e19d
Pratham Patel <prathampatel@thefossguy.com>
2024-07-14 21:33:53 +0530

    rust-manage.sh: use bash as script interpreter and remove fish completions

--- .local/scripts/other-common-scripts/rust-manage.sh
+++ .local/scripts/other-common-scripts/rust-manage.sh
@@ -1,17 +1,19 @@
-#!/usr/bin/env dash
+#!/usr/bin/env bash
 
-set -xeuf
+set -xeuf -o pipefail
 
 RUSTUP_BIN="${1:-$(command -v rustup)}"
-if [ -z "${RUSTUP_BIN}" ]; then
-    >&2 echo 'No rustup? What are you, a monster?'
+COMPLETIONS_DIR="${HOME}/.local/share/bash-completion/completions"
+
+if [[ -z "${RUSTUP_BIN}" ]]; then
+    echo 'No rustup? What are you, a monster?'
     exit 1
 fi
 
-if [ "$(uname -s)" = 'Linux' ]; then
+if [[ "$(uname -s)" == 'Linux' ]]; then
     if pgrep --exact "rust-analyzer|cargo|rustc" > /dev/null; then
-        >&2 echo 'You are probably using components that will be updated, even replaced...'
-        >&2 echo 'Not continuing'
+        echo 'You are probably using components that will be updated, even replaced...'
+        echo 'Not continuing.'
         exit 1
     fi
 fi
@@ -19,13 +21,6 @@ fi
 ${RUSTUP_BIN} default stable
 ${RUSTUP_BIN} update stable
 ${RUSTUP_BIN} component add rust-src rust-analysis rust-analyzer clippy
-
-if echo "${SHELL}" | grep 'bash' > /dev/null; then
-    mkdir -p "${HOME}/.local/share/bash-completion/completions"
-    ${RUSTUP_BIN} completions bash > "${HOME}/.local/share/bash-completion/completions/rustup"
-    ${RUSTUP_BIN} completions bash cargo > "${HOME}/.local/share/bash-completion/completions/cargo"
-elif echo "${SHELL}" | grep 'fish' > /dev/null; then
-    mkdir -p "${HOME}/.config/fish/completions"
-    ${RUSTUP_BIN} completions fish > "${HOME}/.config/fish/completions/rustup.fish"
-    ${RUSTUP_BIN} completions fish cargo > "${HOME}/.config/fish/completions/cargo.fish"
-fi
+mkdir -p "${COMPLETIONS_DIR}"
+${RUSTUP_BIN} completions bash rustup > "${COMPLETIONS_DIR}/rustup"
+${RUSTUP_BIN} completions bash cargo  > "${COMPLETIONS_DIR}/cargo"
