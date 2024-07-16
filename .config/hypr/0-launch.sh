9316d658767597d0ab32abefd58f5a3851e78b5a
Pratham Patel <prathampatel@thefossguy.com>
2024-07-17 00:36:48 +0530

    hyprland: add blueman-applet

--- .config/hypr/0-launch.sh
+++ .config/hypr/0-launch.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-pgrep -x swww        || swww-daemon &
-pgrep -x nm-applet   || nm-applet --indicator & # pkgs.networkmanagerapplet
-pgrep -x waybar      || waybar &
-pgrep -x mako        || mako &
-pgrep -x wl-paste    || wl-paste --watch cliphist store &
+pgrep -x swww           || swww-daemon &
+pgrep -x waybar         || waybar &
+pgrep -x nm-applet      || nm-applet --indicator & # pkgs.networkmanagerapplet
+pgrep -x blueman-applet || blueman-applet & # pkgs.blueman
+pgrep -x mako           || mako &
+pgrep -x wl-paste       || wl-paste --watch cliphist store &
 
 #swww img <IMG>
 notify-send -u normal "Hyprland" "Welcome, $(whoami)"
