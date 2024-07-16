c4c308b026f76ceb1e71440d62147ca4eada9116
Pratham Patel <prathampatel@thefossguy.com>
2024-07-17 00:19:59 +0530

    pipewire was getting fucked by wireplumber, do not start wireplumber

--- .config/hypr/0-launch.sh
+++ .config/hypr/0-launch.sh
@@ -4,15 +4,7 @@ pgrep -x swww        || swww-daemon &
 pgrep -x nm-applet   || nm-applet --indicator & # pkgs.networkmanagerapplet
 pgrep -x waybar      || waybar &
 pgrep -x mako        || mako &
-pgrep -x wireplumber || wireplumber &
 pgrep -x wl-paste    || wl-paste --watch cliphist store &
-
-# workaround for pipewire not working for some fucking reason
-timeout 1 wpctl status
-# we restart pipewire.service only if the command is killed by timeout
-if [[ "$?" -eq 124 ]]; then
-    systemctl --user restart pipewire.service
-fi
 
 #swww img <IMG>
 notify-send -u normal "Hyprland" "Welcome, $(whoami)"
