da631d7d940f0b301db7f25839c314af5e2cbb2e
Pratham Patel <prathampatel@thefossguy.com>
2024-07-16 23:29:00 +0530

    hyprland: apply fix for pipewire issue

--- .config/hypr/0-launch.sh
+++ .config/hypr/0-launch.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
-pgrep swww        || swww init &
-pgrep nm-applet   || nm-applet --indicator & # pkgs.networkmanagerapplet
-pgrep waybar      || waybar &
-pgrep mako        || mako &
-pgrep wireplumber || wireplumber &
-pgrep wl-paste    || wl-paste --watch cliphist store &
+pgrep -x swww        || swww init &
+pgrep -x nm-applet   || nm-applet --indicator & # pkgs.networkmanagerapplet
+pgrep -x waybar      || waybar &
+pgrep -x mako        || mako &
+pgrep -x wireplumber || wireplumber &
+pgrep -x wl-paste    || wl-paste --watch cliphist store &
 
-swww img ~/Pictures/breno-machado-in9-n0JwgZ0-unsplash.jpg
+# workaround for pipewire not working for some fucking reason
+timeout 1 wpctl status
+# we restart pipewire.service only if the command is killed by timeout
+if [[ "$?" -eq 124 ]]; then
+    systemctl --user restart pipewire.service
+fi
+
+#swww img <IMG>
 notify-send -u normal "Hyprland" "Welcome, $(whoami)"
