d16d902d57d2fd7fd41c3352c2a10c162260c10c
Pratham Patel <prathampatel@thefossguy.com>
2024-07-17 10:08:49 +0530

    hyprland: use `pidof` instead of `pgrep -x` where possible

--- .config/hypr/0-launch.sh
+++ .config/hypr/0-launch.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-pgrep -x swww           || swww-daemon &
-pgrep -x waybar         || waybar &
-pgrep -x nm-applet      || nm-applet --indicator & # pkgs.networkmanagerapplet
-pgrep -x blueman-applet || blueman-applet & # pkgs.blueman
-pgrep -x mako           || mako &
-pgrep -x wl-paste       || wl-paste --watch cliphist store &
+pidof -q swww-daemon || swww-daemon &
+pidof -q waybar      || waybar &
+pidof -q nm-applet   || nm-applet --indicator & # pkgs.networkmanagerapplet
+pgrep blueman-applet || blueman-applet & # pkgs.blueman; also, `blueman-applet` is run by `python` :|
+pidof -q mako        || mako &
+pidof -q wl-paste    || wl-paste --watch cliphist store &
 
 #swww img <IMG>
 notify-send -u normal "Hyprland" "Welcome, $(whoami)"
