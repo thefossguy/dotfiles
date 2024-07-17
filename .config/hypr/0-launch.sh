343b0f22669d975d211e13c6e289995eae267342
Pratham Patel <prathampatel@thefossguy.com>
2024-07-17 10:21:39 +0530

    hyprland: start hypridle

--- .config/hypr/0-launch.sh
+++ .config/hypr/0-launch.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-pidof -q swww-daemon || swww-daemon &
+pidof -q hypridle    || hypridle &
+pidof -q wl-paste    || wl-paste --watch cliphist store &
+pidof -q mako        || mako &
 pidof -q waybar      || waybar &
+pidof -q swww-daemon || swww-daemon &
 pidof -q nm-applet   || nm-applet --indicator & # pkgs.networkmanagerapplet
 pgrep blueman-applet || blueman-applet & # pkgs.blueman; also, `blueman-applet` is run by `python` :|
-pidof -q mako        || mako &
-pidof -q wl-paste    || wl-paste --watch cliphist store &
 
 #swww img <IMG>
 notify-send -u normal "Hyprland" "Welcome, $(whoami)"
