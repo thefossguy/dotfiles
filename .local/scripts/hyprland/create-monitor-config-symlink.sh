d38fa09355dd7abc66a9bacd34e51ebea8163473
Pratham Patel <prathampatel@thefossguy.com>
2024-07-20 12:45:20 +0530

    create-monitor-config-symlink.sh: script is called by hyprland so no need to verify if running in hyprland or not

--- .local/scripts/hyprland/create-monitor-config-symlink.sh
+++ .local/scripts/hyprland/create-monitor-config-symlink.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [[ ! -h "${HOME}/.config/hypr/host-monitor.conf" ]] && [[ "${XDG_CURRENT_DESKTOP:-}" == 'Hyprland' ]]; then
+if [[ ! -h "${HOME}/.config/hypr/host-monitor.conf" ]]; then
     pushd "${HOME}/.config/hypr"
     host_monitor_config="host-monitor-configs/$(hostname).conf"
     if [[ ! -f "${host_monitor_config}" ]]; then
@@ -11,4 +11,3 @@ if [[ ! -h "${HOME}/.config/hypr/host-monitor.conf" ]] && [[ "${XDG_CURRENT_DESK
     ln -s "${host_monitor_config}" host-monitor.conf
     popd
 fi
-
