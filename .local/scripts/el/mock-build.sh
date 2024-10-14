4c81224af6f8085a20bb7a9450e0cf70039bb971
Pratham Patel <prathampatel@thefossguy.com>
2024-10-14 11:13:59 +0530

    scripts/mock-build: extract the [ch]"root" name from mock config to create the MOCK_OUT dir

--- .local/scripts/el/mock-build.sh
+++ .local/scripts/el/mock-build.sh
@@ -29,22 +29,32 @@ fi
 # shellcheck disable=SC1090
 source <(grep ID /etc/os-release)
 MARCH="$(uname -m)"
-VERSION_ID="$(source <(grep VERSION_ID /etc/os-release) && awk -F '.' '{print $1}' <<< $VERSION_ID)"
-REAL_MOCK_ROOT="$ID-$VERSION_ID-$MARCH"
-MOCK_ROOT="${MOCK_ROOT:-${REAL_MOCK_ROOT}}"
-MOCK_OUT="$HOME/rpm-mockbuild/${MOCK_ROOT}"
+# shellcheck disable=SC1090,SC2086
+VERSION_ID="$(source <(grep VERSION_ID /etc/os-release) && awk -F '.' '{print $1}' <<< "${VERSION_ID}")"
+SYSTEM_MOCK_ROOT="${ID}-${VERSION_ID}-${MARCH}"
+MOCK_ROOT="${MOCK_CHROOT:-${MOCK_ROOT:-${SYSTEM_MOCK_ROOT}}}"
 
-if [[ ! -d "/var/lib/mock/$MOCK_ROOT" ]]; then
-    mock --root "$MOCK_ROOT" --init
+if [[ -f "/etc/mock/${MOCK_ROOT}.cfg" ]]; then
+    MOCK_ROOT_PATH="/etc/mock/${MOCK_ROOT}.cfg"
+elif [[ -f "/etc/mock/${MOCK_ROOT}" ]]; then
+    MOCK_ROOT_PATH="/etc/mock/${MOCK_ROOT}"
+elif [[ -f "${MOCK_ROOT}" ]]; then
+    MOCK_ROOT_PATH="${MOCK_ROOT}"
+fi
+MOCK_ROOT_NAME="$(grep "config_opts\['root'\]" "${MOCK_ROOT_PATH}" | sed -e "s/'//g" | awk '{print $NF}')"
+
+if [[ ! -d "/var/lib/mock/${MOCK_ROOT_NAME}" ]]; then
+    mock --root "${MOCK_ROOT_PATH}" --init
 fi
 
 additional_args=( "$@" )
+MOCK_OUT="$HOME/rpm-mockbuild/${MOCK_ROOT_NAME}"
 # shellcheck disable=SC2086
 time mock \
     "${additional_args[@]:2}" \
     --isolation nspawn \
-    --root "$MOCK_ROOT" \
-    --resultdir "$MOCK_OUT" \
+    --root "${MOCK_ROOT}" \
+    --resultdir "${MOCK_OUT}" \
     --sources "$1" \
     --no-cleanup-after \
     --buildsrpm \
