55cbacbf11045e069950cdfbb2db06149098217e
Pratham Patel <prathampatel@thefossguy.com>
2024-11-29 17:45:55 +0530

    dotfiles-pull: wait ~13 seconds for internet connectivity before exiting "early"

--- .local/scripts/other-common-scripts/dotfiles-pull.sh
+++ .local/scripts/other-common-scripts/dotfiles-pull.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
-
 set -xeuf -o pipefail
+
+if ! ping -c 1 'gitlab.com' 1>/dev/null 2>&1; then
+    if ! ping -c 2 'gitlab.com' 1>/dev/null 2>&1; then
+        if ! ping -c 20 'gitlab.com' 1>/dev/null 2>&1; then
+            # Exit early because the internet is unreachable
+            exit 0
+        fi
+    fi
+fi
 
 DIRTY_REPO=$(git --git-dir="${HOME}/.dotfiles" --work-tree="${HOME}" status --porcelain=v1 --ignored=no --untracked-files=no)
 if [[ -n "${DIRTY_REPO}" ]]; then
