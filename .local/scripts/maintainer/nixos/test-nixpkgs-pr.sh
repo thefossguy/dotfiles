8e4eb82d0e5727cf1fb3008bda8ce8b3e6509f85
Pratham Patel <prathampatel@thefossguy.com>
2025-12-31 17:02:18 +0530

    scripts/test-nixpkgs-pr: use `nix build` instead of `nom build`

--- .local/scripts/maintainer/nixos/test-nixpkgs-pr.sh
+++ .local/scripts/maintainer/nixos/test-nixpkgs-pr.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -xeuf -o pipefail
 
+reset
 # shellcheck disable=SC2086
-nixpkgs-review pr --print-result --eval local ${EXTRA_ARGS:-} "${1}"
+nixpkgs-review pr --print-result --eval local --build-graph nix ${EXTRA_ARGS:-} "${1}"
