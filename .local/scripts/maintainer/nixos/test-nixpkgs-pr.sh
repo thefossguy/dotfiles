00637228cc224d0f15277fec0013fec7ccf04b2a
Pratham Patel <prathampatel@thefossguy.com>
2026-02-23 19:57:23 +0530

    scripts/test-nixpkgs-pr: do not allow building "broken" packages

--- .local/scripts/maintainer/nixos/test-nixpkgs-pr.sh
+++ .local/scripts/maintainer/nixos/test-nixpkgs-pr.sh
@@ -3,4 +3,4 @@ set -xeuf -o pipefail
 
 reset
 # shellcheck disable=SC2086
-nixpkgs-review pr --print-result --eval local --build-graph nix ${EXTRA_ARGS:-} "${1}"
+nixpkgs-review pr --print-result --eval local --build-graph nix --extra-nixpkgs-config '{ allowBroken = false; }' ${EXTRA_ARGS:-} "${1}"
