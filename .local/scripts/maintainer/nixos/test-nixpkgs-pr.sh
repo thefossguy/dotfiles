b5641e2880c34fe35c0d03560ded89d349f5365a
Pratham Patel <prathampatel@thefossguy.com>
2026-04-22 22:28:42 +0530

    scripts/test-nixpkgs-pr: remove in favour of test-nixpkgs-pr.py

--- .local/scripts/maintainer/nixos/test-nixpkgs-pr.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -xeuf -o pipefail
-
-reset
-# shellcheck disable=SC2086
-nixpkgs-review pr --print-result --eval local --build-graph nix --extra-nixpkgs-config '{ allowBroken = false; }' ${EXTRA_ARGS:-} "${1}"
